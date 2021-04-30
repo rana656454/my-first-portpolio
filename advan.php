6202a677d98e8d7d70c8b2ee32c64b6139ab217a
rana <ranamd1993@gmail.com>
2021-04-30 12:26:04 +0600

    first commit

---
+++ advan.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Advantaures</title>
+		<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,inital-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>My portpolio</title>
+	<!-- Custom css file -->
+	<link rel="stylesheet" type="text/css" href="style_advan.css">
+	<!-- Custom bootstrap file -->
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+	<link href="owl.carousel.min.css" rel="stylesheet">
+	<link href="owl.theme.default.min.css" rel="stylesheet">
+
+	<!-- Custom bootstrap js file -->
+
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.js">
+	<link rel="stylesheet" type="text/css" href="./vender/Magnific-Popup/dist/magnific-popup.css">
+
+<!-- font awesome file -->
+
+      <link rel="stylesheet" type="text/css" href="font-awesome/css/fontawesome.min.css">
+	<!--<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@800&display=swap" rel="stylesheet">-->
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/ed54b3d0ae.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+<header class="hero">
+	<div class="container ttt">
+		<div class="row hero1">
+			<div class="col-lg-12 col-md-6 nav">
+				<div class="row">
+					<div class="col-md-1"><img src="img_a/logo.png"></div>
+					<div class="offset-lg-5 col-lg-6">
+						<!--<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">About</a></li>
+							<li><a href="">Contact</a></li>
+							<li><a href="">Booking</a></li>
+							<li><a href="">destination</a></li>
+							<li><a href="">Place</a></li>
+						</ul>-->
+
+ <nav class="navbar navbar-expand-lg  navbar-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-a" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-a" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-a" href="#">Pricing</a>
+      </li>
+
+       <li class="nav-item">
+        <a class="nav-a" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-a" href="#">Pricing</a>
+      </li>      
+    </ul>
+  </div>
+</nav>
+					</div>
+
+					<div class="col-lg-6">
+				<h3 id="banner-area">The Great &nbsp<span class="element"></span></h3>
+				<h1 class="text-uppercase">Advantaure</h1>
+
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+
+			</div>
+
+			<div class="offset-lg-2 col-lg-4">
+				<div class="row" id="anm">
+				<div class="col-lg-12 up-dw" id="up"></div>
+               <div class="col-lg-12 lt-rt" id="rt"></div>
+               </div>
+			</div>
+               
+
+				</div>
+			</div>
+			
+
+
+		</div>
+	</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,256L48,229.3C96,203,192,149,288,154.7C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,197.3C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+</header>
+
+<section class="">
+<div class="container">
+	<div class="row exp-all">
+		<div class="col-lg-12 exp-head">
+
+           <h4 class="text-uppercase">Experience</h4>
+           <h3 class="text-uppercase">The Thrilling Outdoors</h3>	
+           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.</p>
+
+		</div>
+
+		<div class="col-lg-3 exp-1" id="exp1">
+			<span><i class="fa fa-star" aria-hidden="true"></i>
+				<h6>Star</h6>
+</span>
+<h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen </h5>
+		</div>
+		<div class="col-lg-3 exp-1" id="exp2">
+			<span><i class="fa fa-binoculars" aria-hidden="true"></i>
+				<h6>Hiking</h6>
+</span>
+<h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen </h5>
+			
+		</div>
+		<div class="col-lg-3 exp-1" id="exp3">
+			<span><i class="fa fa-university" aria-hidden="true"></i>
+				<h6>Camping</h6>
+</span>
+<h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen </h5>
+			
+		</div>
+
+			
+
+		
+	</div>
+
+
+</div>
+
+
+</section>
+
+<div id="services">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,128L48,144C96,160,192,192,288,181.3C384,171,480,117,576,106.7C672,96,768,128,864,128C960,128,1056,96,1152,101.3C1248,107,1344,149,1392,170.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
+    <div class="container">
+<h4 class="place">More Beautiful Place</h4>
+
+<div class="owl-carousel owl-theme">
+    <div class="item"><img src="place/1.jpg" width="100px" height="150px">
+     <p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+     
+    </div>
+    <div class="item"><img src="place/2.jpg" width="100px" height="150px">
+    <p class="p-title">Lalbag <br>Dhaka,Bangladesh</p>
+    </div>
+    <div class="item"><img src="place/3.jpg" width="100px" height="150px">
+    <p class="p-title">Rangamati <br>Chittogram,Bangladesh</p>
+ </div>
+    <div class="item"><img src="place/4.jpg" width="100px" height="150px">
+    <p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+ </div>
+    <div class="item"><img src="place/5.jpg" width="100px" height="150px">
+    <p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+ </div>
+    <div class="item"><img src="place/6.jpg" width="100px" height="150px">
+    <p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+ </div>
+    <div class="item"><img src="place/7.jpg" width="100px" height="150px">
+    <p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+ </div>
+    <div class="item"><img src="place/8.jpg" width="100px" height="150px">
+    <p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+ </div>
+    <div class="item"><img src="place/9.jpg" width="100px" height="150px">
+<p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+    </div>
+    <div class="item"><img src="place/10.jpg" width="100px" height="150px">
+<p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+    </div>
+    <div class="item"><img src="place/11.jpg" width="100px" height="150px">
+    <p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+ </div>
+    <div class="item"><img src="place/12.jpg" width="100px" height="150px">
+<p class="p-title">Cox'sbazar <br>Dhaka,Bangladesh</p>
+ </div>
+</div>
+
+    </div>
+
+  </div>
+
+
+<section class="contact" id="cont">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6 inp-head">
+		 <h2>GET IN TOUCH</h2>
+<p>23 Main, Street</p>
+<p>New York, United States</p>
+<p>+88 01912704287</p>
+<p>example@example.com</p>	
+
+<div class="mid"></div>
+		</div>
+
+				<div class="col-lg-6">
+			    <div class="row">
+			    	<div class="col-lg-6">
+			    <input type="text" class="inp1" name="name" placeholder="Your Name">
+			    </div>
+		    	<div class="col-lg-6">
+			    <input type="text" name="name" class="inp1" placeholder="Your Email">
+			    </div>
+                <div class="col-lg-12">
+			    <input type="text" name="name" class="inp2" placeholder="Subject">
+			    </div>
+                  <div class="col-lg-12">
+			    <input type="text" name="name" class="inp2" placeholder="Message">
+			    </div>
+
+			     <div class="col-lg-12">
+			    <input type="button" name="name" class="inp3" value="Send Message">
+			    </div>
+
+			    </div>
+		        </div>
+		
+	</div>
+	
+</div>
+
+</section>
+
+<footer class="foot">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6">
+			<p class="foot-des">rana@copyright2020.com<br>Dhaka,Bangladesh</p>
+
+		</div>
+
+		<div class="col-lg-6">
+		
+     <button type="button"  id="btn1" class="btn btn-social-icon btn-outline-facebook"><i class="fa fa-facebook"></i></button> 
+     <button type="button"  id="btn2" class="btn btn-social-icon btn-outline-youtube"><i class="fay fa fa-youtube"></i></button>
+     <button type="button" id="btn3" class="btn btn-social-icon btn-outline-twitter"><i class="fa fa-twitter"></i></button> 
+     <button type="button" id="btn4" class="btn btn-social-icon btn-outline-linkedin"><i class="fa fa-linkedin"></i></button> 
+     <button type="button" id="btn5" class="btn btn-social-icon btn-outline-instagram"><i class="fai fa fa-instagram"></i></button> 
+
+		</div>
+	</div>
+</div>	
+</footer>
+
+
+
+
+
+<script src="jquery.js"></script>	
+	
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#exp1,#exp2,#exp3').mouseenter(function(){
+			$(this).addClass('jhover');
+		})
+
+
+		$('#exp1,#exp2,#exp3').mouseleave(function(){
+			$(this).removeClass('jhover');
+		})
+
+
+    $('#up').mouseenter(function(){
+			$('#up').toggleClass('upfocus');
+			$('#rt').toggleClass('ltfocus');
+
+		})
+
+
+
+
+	});
+
+</script>
+<script src="owl.carousel.min.js"></script>
+<script type="text/javascript">
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+</script>
+<script type="text/javascript" src="typed.js"></script>
+<script type="text/javascript">
+	
+
+	var typed = new Typed('.element', {
+    strings: ["Outoors", "Visiting Place", "Beautiful Place"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop:true
+});
+</script>
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,561 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,inital-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>My portpolio</title>
+	<!-- Custom css file -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- Custom bootstrap file -->
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+	<!-- Custom bootstrap js file -->
+
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.js">
+	<link rel="stylesheet" type="text/css" href="./vender/Magnific-Popup/dist/magnific-popup.css">
+
+<!-- font awesome file -->
+
+      <link rel="stylesheet" type="text/css" href="font-awesome/css/fontawesome.min.css">
+	<!--<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@800&display=swap" rel="stylesheet">-->
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/ed54b3d0ae.js" crossorigin="anonymous"></script>
+
+	
+</head>
+<body>
+<!--================Start Header Area=================-->
+<header class="header-area">
+	<div class="main-menu">
+		<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#"><img src="img/menu.png" height="50px" width="50px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+  	<div class="mr-auto"></div>
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Portfolio</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pages</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Blog</a>
+      </li>
+ <li class="nav-item">
+        <a class="nav-link" href="#">Contacts</a>
+      </li>
+
+      
+    </ul>
+  </div>
+</nav>
+	</div>
+</header>
+
+
+<!--================End Header Area=================-->
+
+
+
+
+
+
+<!--================Start Main Area=================-->
+
+
+<main class="site-main">
+<!--================Start Banner Area=================-->
+
+<section class="site-banner mainstyle">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-12 site-title">
+				<h3 class="title-text">Hey</h3>
+				<h1 class="title-text text-uppercase">I am Rana</h1>
+				<h4 class="title-text text-uppercase">Try to be a Web Developer</h4>
+				<div class="site-btn">
+
+					<div class="d-flex flex-row flex-wrap">
+						<div class="ssbtn"><button type="button" class="sbtn">Hire Me</button></div>
+						<div class="spbtn"><button type="button" class="pbtn">GET CV</button></div>
+					</div>
+					
+				</div>
+			</div>
+				<div class="col-lg-6 col-md-12 ">
+				<img src="img/banner.png" alt="banner-img" width="550px" height="700px" class="img-fluid">
+			</div>
+		</div>
+		
+	</div>
+	
+</section>
+<!--================End Banner Area=================-->	
+
+
+<!--================Start About Area=================-->	
+
+<section class="about-area mainstyle">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6 col-md-12">
+				<img src="img/cv.png" alt="About us" class="img-fluid" width="550px">
+			</div>
+
+			<div class="col-lg-6 col-md-12">
+                 <h2 class="text-uppercase pt-5">
+                 	<span>Let me</span><br>
+                 	<span>introduce</span><br>
+                 	<span>myself</span>
+                 </h2>
+                   
+
+                   <div class="paragraph py-4 w-75">
+                   	<p class="para">
+                   		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+                   	</p>
+                   	<p class="para">
+                   		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+                   	</p>
+                   </div>
+
+                  <div class="cvbtn"><button type="button" class="cbtn">Download CV</button></div>
+
+				
+			</div>
+
+		</div>
+	</div>
+	
+
+</section>
+
+
+
+<!--================End About Area=================-->	
+
+
+
+
+<!--================Start Brand Area=================-->	
+
+<section class="brand-area mainstyle">
+<div class="container container-fluid">
+	<div class="row">
+		<div class="col-xl-6 col-lg-12 col-md-12">
+		<div class="row">
+			<div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo1.png" alt="brand1" height="100px" width="80px">
+				</div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo2.png" alt="brand2" height="100px" width="80px">
+				</div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo3.jpg" alt="brand3" height="100px" width="80px">
+				</div>
+			</div>
+
+
+
+             <div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo4.jpeg" alt="brand4" height="100px" width="80px">
+				</div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo4.png" alt="brand5" height="100px" width="80px">
+				</div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo8.png" alt="brand6" height="100px" width="80px">
+				</div>
+			</div>
+
+
+                <div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo9.png" alt="brand7" height="100px" width="80px">
+				</div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo7.png" alt="brand8" height="100px" width="80px">
+				</div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-6">
+				<div class="single-brand">
+					<img src="img/logo10.png" alt="brand9" height="100px" width="80px">
+				</div>
+			</div>
+
+		</div>
+
+		</div>	
+
+
+<div class="col-xl-6 col-lg-12 col-md-12">
+			<div class="experience-area">
+				<div class="d-flex flex-row years-area">
+					<h2 class="p-3 years">08</h2>
+
+					<h2>
+						<span class="text-uppercase">Years</span><br>
+						<span class="text-uppercase">Experience</span><br>
+						<span class="text-uppercase">Working</span>
+						
+					</h2>
+					
+				</div>
+
+
+				<div class="d-flex flex-row flex-wrap call-area">
+					<span><i class="fa fa-phone" aria-hidden="true"></i></span>
+
+					<div class="call-now">
+
+						<a href="">Call Now</a><br>
+						<span>(+88)01771656454</span>
+					</div>
+					
+				</div>
+
+				<div class="bg-color">
+					
+				</div>
+				
+			</div>
+		</div>
+		
+
+
+		</div>
+
+		
+        
+
+	</div>
+		
+
+</section>
+
+
+<!--================End Brand Area=================-->	
+
+
+
+<!--================Start Project Area=================-->	
+
+<section class="project-area mainstyle">
+
+	<div class="container">
+<div class="project-title pb-5">
+	<h1 class="text-uppercase title-h1">Recently Done Project</h1>
+	<h1 class="text-uppercase title-h1">Quality Work</h1>
+</div>
+  <div class="button-group">
+  	<button type="button" class="active" id="btn1">All</button>
+  	<button type="button" data-filter=".popular">Popular</button>
+  	<button type="button" data-filter=".latest">Latest</button>
+  	<button type="button" data-filter=".follow">Following</button>
+  	<button type="button" data-filter=".upcom">Upcoming</button>
+  </div>
+
+		<div class="row grid">
+			<div class="col-lg-4 col-md-6 col-sm-12 element-item latest">
+				<div class="our-project">
+					    <div class="img">
+						<a class ="test-popup-link" href="./img/project/1.jpg">
+						<img src="img/project/1.jpg" height="200px" width="270px">	
+						</a>
+						
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Latest,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			</div>
+
+
+              <div class="col-lg-4 col-md-6 col-sm-12 element-item popular">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/2.jpg">
+						<img src="img/project/2.jpg" height="200px" width="270px">	
+						</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Popular,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+
+              <div class="col-lg-4 col-md-6 col-sm-12 element-item popular">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/3.jpg">
+						<img src="img/project/3.jpg" height="200px" width="270px">	
+						</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Popular,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+
+
+              <div class="col-lg-4 col-md-6 col-sm-12 element-item follow">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/4.jpg">
+						<img src="img/project/4.jpg" height="200px" width="270px">
+						</a>
+						
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Following,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+
+
+
+              <div class="col-lg-4 col-md-6 col-sm-12 element-item follow">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/5.jpg">
+						<img src="img/project/5.jpg" height="200px" width="270px">
+					</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Following,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+              <div class="col-lg-4 col-md-6 col-sm-12 element-item follow">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/6.jpg">
+						<img src="img/project/6.jpg" height="200px" width="270px">
+					</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Following,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+             <div class="col-lg-4 col-md-6 col-sm-12 element-item upcom">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/7.jpg">
+						<img src="img/project/7.jpg" height="200px" width="270px">
+					</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Upcoming,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+
+
+
+              <div class="col-lg-4 col-md-6 col-sm-12 element-item upcom">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/8.jpg">
+						<img src="img/project/8.jpg" height="200px" width="270px">
+					</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Upcoming,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+              <div class="col-lg-4 col-md-6 col-sm-12 element-item upcom">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/9.jpg">
+						<img src="img/project/9.jpg" height="200px" width="270px">
+					</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Upcoming,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+                <div class="col-lg-4 col-md-6 col-sm-12 element-item upcom">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/10.jpg">
+						<img src="img/project/10.jpg" height="200px" width="270px">
+					</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Upcoming,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+              <div class="col-lg-4 col-md-6 col-sm-12 element-item upcom">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/11.jpg">
+						<img src="img/project/11.jpg" height="200px" width="270px">
+					</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Upcoming,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+             <div class="col-lg-4 col-md-6 col-sm-12 element-item upcom">
+				<div class="our-project">
+					<div class="img">
+						<a class ="test-popup-link" href="./img/project/12.jpg">
+						<img src="img/project/12.jpg" height="200px" width="270px">
+					</a>
+					</div>
+					<div class="title py-4">
+						<h4 class="text-uppercase">Minimul Design</h4>
+						<span class="text-uppercase">Upcoming,Portfolio</span>
+						
+					</div>
+
+				</div>
+				
+			    </div>
+
+
+
+
+
+
+			
+		</div>
+
+
+
+
+
+
+	</div>
+</section>
+
+
+<!--================End Project Area=================-->	
+
+
+
+
+
+	
+</main>
+
+
+<!--=================End Main Area==================-->
+
+
+
+
+<script src="jquery.js"></script>	
+<script src="bootstrap.min.js"></script>
+<script src="./vender/isotope/isotope.min.js"></script>
+<script type="text/javascript" src="./vender/Magnific-Popup/dist/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="./js/main.js"></script>
+
+</body>
+</html>
